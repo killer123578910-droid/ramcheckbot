532b722349364897c0c84c07a8cfa0b59bdef4d7
Khuong <killer123578910@gmail.com>
2026-07-08 21:23:39 +0700

    temporal simulated

--- main.py
+++ main.py
@@ -1,46 +1,6 @@
-from datetime import datetime
-import requests
-import os
-from dotenv import load_dotenv
-from taobang import create_database,insert
-def crawling():
-    crawldatetime=datetime.now().isoformat()
-    kq1={"Ram":[],
-         "SSD":[],
-         "VGA":[],
-         "MAIN":[],
-         "crawl_time":crawldatetime}
-    for key,value in matra.items():
-        kq=[]
-        count=1
-        while(True):
-            ulr=f'{baseu}&page={count}&category={key}'
-            data =requests.get(ulr,headers=header)
-            datae = data.json()
-            sp=datae["list"]
-            if not sp:
-                break
-            kq.extend(sp)
-            count+=1
-        kq1[value]=kq
-    return kq1
-load_dotenv()
-key=os.getenv("api")
-baseu=os.getenv("base_url")
-header={
-    "accept": "*/*",
-    "accept-language": "en-US,en;q=0.9",
-    "authorization": "Basic ssaaAS76DAs6faFFghs1",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36",
-    "x-requested-with": "XMLHttpRequest"
-}
-
-matra={
-    283:"Ram",
-    284:"SSD",
-    279:"VGA",
-    278:"MAIN"}
-
+from maindef import crawling
+from taobang import create_database,insert,simulate7days
 create_database()
 insert(crawling())
+#simulate7days()
 
--- taobang.py
+++ taobang.py
@@ -1,4 +1,6 @@
 import sqlite3
+import random
+from datetime import datetime,timedelta
 def create_database():
     conn = sqlite3.connect("product.db")
     cursor = conn.cursor()
@@ -76,26 +78,57 @@ def insert(arr):
         if key!="crawl_time":
             for i in value:
                 name        = i.get("productName") or ""
-                price       = i.get("price") or 0
+                price       = i.get("price")
                 quantity    = i.get("quantity") or 0
                 visiting    = i.get("visit") or 0
                 source_url  = i.get("productUrl") or ""
                 rating      = i.get("rating")
+                
+                
+                if price!=0:
+                    cursor.execute(upsert_sql,(name,quantity,visiting,source_url,key,crawltime))
+                
+                
+                
+                    product_id=cursor.lastrowid
+                    if product_id==0:
+                        cursor.execute("SELECT id FROM products where source_url = ?",(source_url,))
+                        row=cursor.fetchone()
+                        product_id=row[0] if row else None
+                    if product_id:
+                        cursor.execute(snapshot_sql,(product_id,crawltime,price,visiting,rating))
+    conn.commit()
+    conn.close()
+    
+def simulate7days():
+    conn = sqlite3.connect("product.db")
+    cursor = conn.cursor()
+    ngaydau=datetime.strptime("2026-07-08T21:20:43.862753","%Y-%m-%dT%H:%M:%S.%f")
+    cursor.execute("SELECT product_id,prices,view,rating FROM products_snapshot where time=?",("2026-07-08T21:20:43.862753",))
+    productid=cursor.fetchall()
+    
+    
+    snapshot_sql ="""
+        INSERT INTO products_snapshot (product_id,time,prices,view,rating) VALUES (?, ?, ? ,? ,?)
+    """
+    
+    
+    n=1
+    for i in range(7):
+        
+        
+        
+        ngayht=ngaydau+ timedelta(days=n)
+        for product_id,prices,view,rating in productid:
+            thaydoigia=random.uniform(-0.3,0.3)
+            thaydoiview=random.uniform(0.01,0.2)
+            thaydoirating=random.uniform(-0.1,0.1)
             
-                if not source_url:
-                    print(f"⚠️ Bỏ qua sản phẩm không có URL: {name}")
-                    continue
-                
-                cursor.execute(upsert_sql,(name,quantity,visiting,source_url,key,crawltime))
-                
-                
-                
-                product_id=cursor.lastrowid
-                if product_id==0:
-                    cursor.execute("SELECT id FROM products where source_url = ?",(source_url,))
-                    row=cursor.fetchone()
-                    product_id=row[0] if row else None
-                if product_id:
-                    cursor.execute(snapshot_sql,(product_id,crawltime,price,visiting,rating))
+            nprice=int(prices+prices*thaydoigia)
+            nview=int(view+view*thaydoiview)
+            nrating=float(f"{rating+rating*thaydoirating:.1f}")
+            
+            cursor.execute(snapshot_sql,(product_id,ngayht,nprice,nview,nrating))
+        n+=1
     conn.commit()
     conn.close()
